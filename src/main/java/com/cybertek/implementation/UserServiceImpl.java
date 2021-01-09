d4802afce4ca0fb5a5686bc9c18ca9f1c5a5fc33
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-01-09 14:12:11 -0500

    delete 2

--- src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -63,6 +63,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(String username) {
+        User user = userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
 
     }
 
