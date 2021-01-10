f3cf2f335d3b527cc0ad2eb8fecf8055df796180
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-01-10 16:46:53 -0500

    last for today

--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -3,6 +3,7 @@ package com.cybertek.implementation;
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.entity.Project;
 import com.cybertek.mapper.ProjectMapper;
+import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
 import com.cybertek.utils.Status;
@@ -15,10 +16,12 @@ public class ProjectServiceImpl implements ProjectService {
 
     private ProjectMapper projectMapper;
     private ProjectRepository projectRepository;
+    private UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectMapper projectMapper, ProjectRepository projectRepository) {
+    public ProjectServiceImpl(ProjectMapper projectMapper, ProjectRepository projectRepository, UserMapper userMapper) {
         this.projectMapper = projectMapper;
         this.projectRepository = projectRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -35,6 +38,7 @@ public class ProjectServiceImpl implements ProjectService {
     public void save(ProjectDTO dto) {
         dto.setProjectStatus(Status.OPEN);
         Project obj = projectMapper.convertToEntity(dto);
+        obj.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
         Project project = projectRepository.save(obj);
     }
 
