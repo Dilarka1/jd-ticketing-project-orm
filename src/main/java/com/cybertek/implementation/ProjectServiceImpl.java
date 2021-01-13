da2ebbdff42b153d82736f93a36221065033f22a
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-01-13 12:37:07 -0500

    new day

--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -28,7 +28,8 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectDTO getByProjectCode(String code) {
-        return null;
+        Project project = projectRepository.findByProjectCode(code);
+        return projectMapper.convertToDto(project);
     }
 
     @Override
@@ -49,14 +50,26 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public ProjectDTO update(ProjectDTO dto) {
-        return null;
+    public void update(ProjectDTO dto) {
+        Project project = projectRepository.findByProjectCode(dto.getProjectCode());
+        Project convertedProject = projectMapper.convertToEntity(dto);
+        convertedProject.setId(project.getId());
+        convertedProject.setProjectStatus(project.getProjectStatus());
+        projectRepository.save(convertedProject);
     }
 
     @Override
     public void delete(String code) {
         Project project = projectRepository.findByProjectCode(code);
         project.setIsDeleted(true);
+        projectRepository.save(project);
+
+    }
+
+    @Override
+    public void complete(String projectCode) {
+        Project project = projectRepository.findByProjectCode(projectCode);
+        project.setProjectStatus((Status.COMPLETE));
         projectRepository.save(project);
 
     }
